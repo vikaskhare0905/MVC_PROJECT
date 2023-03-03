bc9cb49120673b9a15a3ea4fb35caea6cff2f721
vikaskhare0994 <vikaskharevk1994@gmail.com>
2023-03-03 19:39:03 +0530

    Changes by vikas

---
+++ src/test/java/com/BikkadIT/SpringMvcFirstApp/SpringMvcFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SpringMvcFirstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
